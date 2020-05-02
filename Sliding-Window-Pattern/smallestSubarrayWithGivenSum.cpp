0ef02bd2f59c5ea7bfd73198f63f884f7e09d0fb
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-05-02 11:31:08 +0530

    .gitignore check

---
+++ Sliding-Window-Pattern/smallestSubarrayWithGivenSum.cpp
@@ -0,0 +1,32 @@
+/*
+ * Created Date: Saturday, May 2nd 2020, 11:14:55 am
+ * Author: Kiran Babu Muddam
+ 
+Problem Statement #
+Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
+
+Example 1:
+
+Input: [2, 1, 5, 2, 3, 2], S=7 
+Output: 2
+Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+Example 2:
+
+Input: [2, 1, 5, 2, 8], S=7 
+Output: 1
+Explanation: The smallest subarray with a sum greater than or equal to '7' is [8].
+Example 3:
+
+Input: [3, 4, 1, 1, 6], S=8 
+Output: 3
+Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6].
+
+Time Complexity : 
+Space Complexity :
+ * 
+ */
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"All is well"<<endl;
+}
