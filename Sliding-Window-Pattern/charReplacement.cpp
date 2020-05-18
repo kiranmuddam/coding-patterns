105fc9748e713bd5f9bba8d54330ffa0e99d6b19
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-05-18 10:59:38 +0530

    add time complexity and space complexity

--- Sliding-Window-Pattern/charReplacement.cpp
+++ Sliding-Window-Pattern/charReplacement.cpp
@@ -19,6 +19,9 @@ Example 3:
 Input: String="abccde", k=1
 Output: 3
 Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+
+Time Complexity : O(n)
+Space Complexity: O(n)
  */
 #include<iostream>
 #include<string>
